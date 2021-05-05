0523e29c1d2358e769da7f389a62ae460748dab3
arslan555 <mirzaarslan450@gmail.com>
2021-05-05 21:21:11 +0500

    added problem set for each file

--- ProblemThree.cpp
+++ ProblemThree.cpp
@@ -1,7 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define NO_OF_CHARS 256
+/*
+Given a string S, find length of the longest substring with all distinct characters.
+Example 1:
+Input:
+S = "geeksforgeeks"
+Output: 7
+Explanation: "eksforg" is the longest
+substring with all distinct characters.
+​Example 2:
+Input:
+S = "aaa"
+Output: 3
+Explanation: "a" is the longest substring
+with all distinct characters.
+Your Task: You don't need to read input or print anything. Your task is to complete the function
+longestSubstrDitinctChars() which takes the string S as input and returns the length of the longest
+substring with all distinct characters.
 
+Expected Time Complexity: O(|S|).Expected Auxiliary Space: O(1).
+Constraints:1<=|S|<=105
+*/
 int longestUniqueSubstring(string str)
 {
     int n = str.size();
--- ProblemTwo.cpp
+++ ProblemTwo.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
+/*
+Given an array A of n positive numbers. The task is to find the first Equilibium Point in the array.
+Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements
+after it.
+Example 1:
+Input:
+n = 1
+A[] = {1}
+Output: 1
+Explanation: Since its the only
+element hence its the only equilibrium
+point.
+Example 2:
+Input:
+n = 5
+A[] = {1,3,5,2,2}
+Output: 3
+Explanation: For second test case
+equilibrium point is at position 3
+as elements before it (1+3) =
+elements after it (2+2).
+
+Your Task:The task is to complete the function equilibriumPoint() which takes the array and n as input
+parameters and returns the point of equilibrium. Return -1 if no such point exists.
+
+Expected Time Complexity: O(n) Expected Auxiliary Space: O(1)
+Constraints:1 <= n <= 1061 <= A[i] <= 108
+
+*/
+
 
 int equilibriumPoint(int Array[], int n){
-
 int midOfArray = n/2;
 int current = midOfArray;
 
